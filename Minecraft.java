24a40ee938df8a3e223b295c7ff91e5095a3734d
danielnguyen446 <77251383+danielnguyen446@users.noreply.github.com>
2021-02-16 16:58:57 -0800

    Update Minecraft.java

--- Minecraft.java
+++ Minecraft.java
@@ -1,6 +1,29 @@
-public class Minecraft
+/**
+ * Name: Daniel Nguyen
+ * ID: A16129027
+ * Email: d7nguyen@ucsd.edu
+ * Sources used: none
+ * 
+ * This file contains code that demonstrates the use of 2d arrays and array
+ * lists.
+ */
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * This class contains two methods. The first rotates a floor plan and the 
+ * other returns the names of mobs that came into contact with an infected mob.
+ */
+public class minecraft
 {
-    public static int[][] rotateFloorPlan(int[][] originalFloorPlan);
+    /**
+     * modifies a 2d array containing blocks on the floor by rotating it 90
+     * degrees clockwise
+     * 
+     * @returns the new array
+     */
+    public static int[][] rotateFloorPlan(int[][] originalFloorPlan)
     {
         //set dimension variables to the height and width of the original plan
         int dimensionA = originalFloorPlan.length;
@@ -9,7 +32,7 @@ public class Minecraft
         //create a new floor plan. Height and width will be swapped
         int[][] newFloorPlan = new int[dimensionB][dimensionA];
         
-        for(int row=0; row<dimensionA; r++)
+        for(int row=0; row<dimensionA; row++)
         {
             //loop for each row
             for(int column=0; column<dimensionB; column++)
@@ -25,26 +48,35 @@ public class Minecraft
         return newFloorPlan;
     }
     
+    
+    /**
+     * Identifies all rows in a 2d array that contain the name of an
+     * infected mob
+     * 
+     * @returns the strings that were in those rows in an array.
+     *
     public static ArrayList<String> getMobsToTest(String[][] groups, String infected);
     {
         //the list of mobs to test. Return this list.
         String [] mobsToTest;
         
-        for(each column)
+        for(int row=0; a<groups.length; row++)
         {
-            for(each row)
+            if(groups[row].contains(infected) == true)
             {
-                if(groups[][] == infected)
-                {
-                    //add every string in the row to mobsToTest
+                //add every string in the row to mobsToTest
                     
-                }
             }
         }
         return mobsToTest;
+    }*/
+    
+
+    public static void main(String[] args)
+    {
+        
+        
+        
+
     }
-	public static void main(String[] args)
-	{
-		
-	}
 }
